ee2764c5de8f81b3bb8ca4f2318b4e0ed73f606c
Tasin1025 <tasintt@gmail.com>
2023-08-15 00:45:20 +0600

    footer added

--- adminnavbar.php
+++ adminnavbar.php
@@ -1,17 +1,4 @@
-<?php 
-// session_start();
-//?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="./assets/css/navStyle.css">
-</head>
-<body>
     <nav>
         <ul>
         <li><a href="showOrder.php">Orders</a></li>
@@ -30,5 +17,4 @@
          header("location: ./login.php");
         }
         ?>
-</body>
 </html>
